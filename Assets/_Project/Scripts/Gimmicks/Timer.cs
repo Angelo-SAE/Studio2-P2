a9e567d7f676284c066c6de95c25d5f1ef5df59f
Mohammad Thani <mohammad.almheiri@student.saedubai.com>
2024-02-17 19:29:42 +0400

    Finished all my levels

--- Assets/_Project/Scripts/Gimmicks/Timer.cs
+++ Assets/_Project/Scripts/Gimmicks/Timer.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public float timer = 3;
     public PlayerMovement player;
+    public Animator animate;
 
-    void Update()
+    public void Update()
     {
         timer -= Time.deltaTime;
         Debug.Log(timer);
+        string levelNumber = SceneManager.GetActiveScene().name;
+
+        if (timer <= 0)
+        {
+            if (levelNumber == "Level 8")
+            {
+                SceneManager.LoadScene("Level 8");
+            }
+
+
+            if (levelNumber == "Level 10")
+            {
+                SceneManager.LoadScene("Level 10");
+            }
+        }
     }
 
-    void TimerReset()
+    public void TimerReset()
     {
-        timer = 10;
-        myAnimatorController.Play("Countdown", -1, normalizedTime = 0.0f);
+        timer = 3;
+        Animator animate = gameObject.GetComponent<Animator>();
+        animate.Rebind();
+        animate.Update(0f);
     }
 }
